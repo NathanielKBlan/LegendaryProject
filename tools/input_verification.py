898f905bf62f5d73eafc45b61d8c1256998518ea
NathanielKBlan <nateblanquel@gmail.com>
2019-11-14 13:17:08 -0500

    Added input_verification module

--- tools/input_verification.py
+++ tools/input_verification.py
@@ -1,15 +1,24 @@
 #This module contains various methods that can be used for input verification
 
 #This method acts as a switch statment and pairs certain values with cases
-def switch(value, arg*):
+def switch(value, keys, *arg):
 
-    switchingCases = {}
-    for i in range(len(arg)):
-        switchingCases[i + 1] = arg[i]
+	switchingCases = {}
+	switchingCasesNums = {}
 
-    if(value > len(arg) or value < 1):
-        newVal = int(input("That was invalid input, please enter a new option: "))
-        return None
-    else:
-        case = switchingCases.get(value)
-        return case
+	for i in range(len(arg)):
+		switchingCases[keys[i]] = arg[i]
+
+	for i in range(len(arg)):
+		switchingCasesNums[i] = arg[i]
+
+	if(value not in keys):
+		if(int(value) < 0 or int(value) > len(arg)):
+			newVal = input("That was invalid input, please enter a new option: ")
+			return switch(newVal, keys, arg)
+		else:
+			case = switchingCasesNums.get(int(value) - 1)
+			return case
+	else:
+		case = switchingCases.get(value)
+		return case
