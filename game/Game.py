c4c3cc9f4048181e4075b164cf11d041d38b1913
NathanielKBlan <nateblanquel@gmail.com>
2019-11-23 11:19:13 -0500

    Created a game package, this is where all game progression files will go

---
+++ game/Game.py
@@ -0,0 +1,9 @@
+class Game:
+
+    def __init__(self, gameName):
+        self.__gameName = gameName
+        self.__saveDirectory = "Set save directory here"
+
+    def startGame(self):
+        # Write implementation for code here
+        pass
