3f96004c7b4e0c471c7d2b060ddc0b1dc1dd3092
NathanielKBlan <nateblanquel@gmail.com>
2019-11-26 14:19:59 -0500

    Added Ordinary class

--- classes/Ordinary.py
+++ classes/Ordinary.py
@@ -1,14 +1,18 @@
 #This is the default player class, can be chosen if so wished
 class Ordinary:
 
-    def __init__(self):
-        self.__health = 25
-        self.__charisma = 10
-        self.__strength = 10
-        self.__intelligence = 10
-        self.__resourcefulness = 10
-        self.__luck = 10
+    def __init__(self, health = 25, charisma = 10, strength = 10, intelligence = 10, resourcefulness = 10, faith = 10, luck = 10):
+        self.__health = health
+        self.__charisma = charisma
+        self.__strength = strength
+        self.__intelligence = intelligence
+        self.__resourcefulness = resourcefulness
+        self.__faith = faith
+        self.__luck = luck
+        self.__inventory = []
 
+    #The following methods are getters and setters, all of these will be inherited by other classes
+    #Can be overwritten if so wished
     def getHealth(self):
         return self.__health
 
@@ -27,3 +31,29 @@ class Ordinary:
     def getLuck(self):
         return self.__luck
 
+    def getFaith(self):
+        return self.__faith
+
+    def setHealth(self, newHealth):
+        self.__health = newHealth
+
+    def setCharisma(self, newCharmisma):
+        self.__charisma = newCharmisma
+
+    def setStrength(self, newStrength):
+        self.__strength = newStrength
+
+    def setIntelligence(self, newIntelligence):
+        self.__intelligence = newIntelligence
+
+    def setResourcefulness(self, newResourcefulness):
+        self.__resourcefulness = newResourcefulness
+
+    def setLuck(self, newLuck):
+        self.__luck = newLuck
+
+    def setFaith(self, newFaith):
+        self.__faith = newFaith
+
+    def addToInventory(self, item):
+        self.__inventory.append(item)
