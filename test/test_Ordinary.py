138ef458e875f209ad78d011f6096cc7adfe4ac4
theIdealist <ychou@fandm.edu>
2019-11-29 03:36:57 -0500

    Test module for ordinary class

--- test/test_Ordinary.py
+++ test/test_Ordinary.py
@@ -13,9 +13,17 @@ def test():
 	#Test setters
 	a.setHealth(20)
 	print("New health:", a.getHealth())
+
 	a.setCharisma(100)
 	print("New charisma:", a.getCharisma())
+
 	a.addToInventory("Golden armour")
+	
+	a.takeDamage(20)
+	print(a.getHealth())
+	
+	print(a.getStrength())
+	a.attack('Pikachu')
 
 def main():
 	test()
